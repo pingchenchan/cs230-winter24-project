8ed4920bc6533064ad145232ab8b5dae943c000e
Terrylin2023 <148923652+Terrylin2023@users.noreply.github.com>
2024-03-13 15:12:39 -0700

    two scripts added
    data1
    data2
    stop

--- app/app.py
+++ app/app.py
@@ -15,8 +15,22 @@ def home():
     <head>
         <title>Home Page</title>
         <script>
-            async function startDataCollection() {
-                const response = await fetch('/start_collect');
+            async function startDataCollection1() {
+                const response = await fetch('/start_collect1');
+                const data = await response.json();
+                alert(data.message); 
+                updateProgress();
+            }
+
+            async function startDataCollection2() {
+                const response = await fetch('/start_collect2');
+                const data = await response.json();
+                alert(data.message); 
+                updateProgress();
+            }
+
+            async function stopDataCollection() {
+                const response = await fetch('/stop_collect');
                 const data = await response.json();
                 alert(data.message); 
             }
@@ -28,7 +42,7 @@ def home():
                         document.getElementById('status').innerText = `Status: ${data.status}`;
                         document.getElementById('cpu_usage').innerText = `CPU Usage: ${data.cpu_usage}%`;
                         document.getElementById('memory_usage').innerText = `Memory Usage: ${data.memory_usage}%`;
-                        document.getElementById('throughput').innerText = `Throughput: ${data.throughput} kbps`;
+                        document.getElementById('throughput').innerText = `Throughput: ${data.throughput} mbps`;
                         document.getElementById('health').innerText = `Health: ${data.health}`;
                         document.getElementById('message').innerText = data.message;
                     })
@@ -36,17 +50,19 @@ def home():
             }
 
             document.addEventListener('DOMContentLoaded', () => {
-                setInterval(updateProgress, 1000); 
+                setInterval(updateProgress, 1000); // Update progress every 1 second
             });
         </script>
     </head>
     <body>
-        <h1>This is a Flask InfluxDB Integration Example!</h1>
-        <button onclick="startDataCollection()">Start Data Collection</button>
+        <h1>This is an Example!</h1>
+        <button onclick="startDataCollection1()">Start Data_1 Collection</button>
+        <button onclick="startDataCollection2()">Start Data_2 Collection</button>
+        <button onclick="stopDataCollection()">Stop Data Collection</button>
         <p id="status">Status: idle</p>
         <p id="cpu_usage">CPU Usage: 0%</p>
         <p id="memory_usage">Memory Usage: 0%</p>
-        <p id="throughput">Throughput: 0 kbps</p>
+        <p id="throughput">Throughput: 0 mbps</p>
         <p id="health">Health: unknown</p>
         <p id="message">Click start to collect data.</p>
     </body>
@@ -60,20 +76,24 @@ kafka_topic = os.getenv('KAFKA_TOPIC', 'server_test')
 # Create a Kafka producer
 producer = KafkaProducer(bootstrap_servers=[kafka_broker],value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-# 用於儲存進度
+# Progress of data collection
 progress = {"status": "idle", "cpu_usage": 0, "memory_usage": 0,"throughput": 0,"health": "unknown", "message": ""}
+stop_collect_flag = True
 
 
 
 
-def collect_and_send_data():
+def collect_and_send_data1():
     global progress
-    # 定時收集和發送數據
-    for i in range(10):
-        cpu_usage = i * 9
-        memory_usage= i * random.randint(1, 10)
-        throughput = i * random.randint(10, 100)
-        health = "healthy" if cpu_usage < 80 else "unhealthy"
+    cpu_usage = 0 
+    while cpu_usage < 80 and not stop_collect_flag:
+        if cpu_usage > 70:
+            cpu_usage = random.randint(70, 75)
+        else:
+            cpu_usage += random.randint(1, 5)
+        memory_usage= random.randint(10, 80)
+        throughput = random.randint(100, 500)
+        health = "healthy" if cpu_usage < 70 else "unhealthy"
         # Create data point
         data_point = {
             "cpu_usage": cpu_usage,
@@ -83,29 +103,34 @@ def collect_and_send_data():
         }
         # Send data point to Kafka topic
         producer.send(kafka_topic, value=data_point)
-        print(f"Sent to Kafka -> CPU usage: {cpu_usage}%, Memory usage: {memory_usage}%, Throughput: {throughput} kbps, Health: {health}")
+        print(f"Sent to Kafka -> CPU usage: {cpu_usage}%, Memory usage: {memory_usage}%, Throughput: {throughput} mbps, Health: {health}")
         progress = {
             "status": "collecting",
             "cpu_usage": cpu_usage,
             "memory_usage": memory_usage,
             "throughput": throughput,
             "health": health,
-            "message": f"Sent to Kafka -> CPU usage: {cpu_usage}%, Memory usage: {memory_usage}%, Throughput: {throughput} kbps, Health: {health}"
+            "message": f"Sent to Kafka -> CPU usage: {cpu_usage}%, Memory usage: {memory_usage}%, Throughput: {throughput} mbps, Health: {health}"
         }
-        time.sleep(1)  # 每1秒運行一次
-    progress["status"] = "complete"
-    progress["message"] = "Data collection and sending complete!"
+        time.sleep(1)  
+    if stop_collect_flag:
+        progress["status"] = "stopping"
+        progress["message"] = "Data collection stopped."
+    else:
+        progress["status"] = "complete"
+        progress["message"] = "Data collection and sending complete!"
 
-def collect_and_send_data_forever():
-    print("collect_and_send_data_forever")
+def collect_and_send_data2():
     global progress
-    # 定時收集和發送數據
-    while True:
-        i = random.randint(1, 10)
-        cpu_usage = i * 9
-        memory_usage= i * random.randint(1, 10)
-        throughput = i * random.randint(10, 100)
-        health = "healthy" if cpu_usage < 80 else "unhealthy"
+    cpu_usage = 0
+    while cpu_usage <35 and not stop_collect_flag:
+        if cpu_usage > 20:
+            cpu_usage = random.randint(20, 30)
+        else:
+            cpu_usage += random.randint(1, 5)
+        memory_usage= random.randint(10, 80)
+        throughput = random.randint(100, 500)
+        health = "healthy" if cpu_usage < 70 else "unhealthy"
         # Create data point
         data_point = {
             "cpu_usage": cpu_usage,
@@ -115,25 +140,54 @@ def collect_and_send_data_forever():
         }
         # Send data point to Kafka topic
         producer.send(kafka_topic, value=data_point)
-        print(f"Sent to Kafka -> CPU usage: {cpu_usage}%, Memory usage: {memory_usage}%, Throughput: {throughput} kbps, Health: {health}")
+        print(f"Sent to Kafka -> CPU usage: {cpu_usage}%, Memory usage: {memory_usage}%, Throughput: {throughput} mbps, Health: {health}")
         progress = {
             "status": "collecting",
             "cpu_usage": cpu_usage,
             "memory_usage": memory_usage,
             "throughput": throughput,
             "health": health,
-            "message": f"Sent to Kafka -> CPU usage: {cpu_usage}%, Memory usage: {memory_usage}%, Throughput: {throughput} kbps, Health: {health}"
+            "message": f"Sent to Kafka -> CPU usage: {cpu_usage}%, Memory usage: {memory_usage}%, Throughput: {throughput} mbps, Health: {health}"
         }
-        print("sent to kafka")
-        time.sleep(2)  # 每1秒運行一次
- 
-@app.route('/start_collect')
-def start_collect():
-    global progress
-    progress = {"status": "starting", "cpu_usage": 0, "memory_usage": 0,"throughput": 0,"health": "unknown", "message": "Starting data collection..."}
-    thread = threading.Thread(target=collect_and_send_data)
-    thread.start()
-    return jsonify({"message": "Data collection started."})
+        time.sleep(1)
+    if stop_collect_flag:
+        progress["status"] = "stopping"
+        progress["message"] = "Data collection stopped."
+    else:
+        progress["status"] = "complete"
+        progress["message"] = "Data collection and sending complete!"
+
+@app.route('/start_collect1')
+def start_collect1():
+    global progress, stop_collect_flag
+    if stop_collect_flag:
+        stop_collect_flag = False 
+        progress = {"status": "starting", "cpu_usage": 0, "memory_usage": 0,"throughput": 0,"health": "unknown", "message": "Starting data collection..."}
+        thread = threading.Thread(target=collect_and_send_data1)
+        thread.start()
+        return jsonify({"message": "Data collection started."})
+    else:
+        return jsonify({"message": "Please stop the current data collection first."})
+@app.route('/start_collect2')
+def start_collect2():
+    global progress,stop_collect_flag
+    if stop_collect_flag:
+        stop_collect_flag = False 
+        progress = {"status": "starting", "cpu_usage": 0, "memory_usage": 0,"throughput": 0,"health": "unknown", "message": "Starting data collection..."}
+        thread = threading.Thread(target=collect_and_send_data2)
+        thread.start()
+        return jsonify({"message": "Data collection started."})
+    else:
+        return jsonify({"message": "Please stop the current data collection first."})
+
+@app.route('/stop_collect')
+def stop_collect():
+    global stop_collect_flag
+    stop_collect_flag = True
+    progress["status"] = "stopping"
+    progress["message"] = "Stopping data collection..."
+    return jsonify({"message": "Data collection stopped."})
+
 
 @app.route('/progress')
 def get_progress():
@@ -147,10 +201,6 @@ def health_check():
     else:
         return jsonify({'status': 'Unhealthy'}),503
     
-def delayed_start():
-    time.sleep(5)
-    collect_and_send_data_forever()
+
 if __name__ == '__main__':
-    thread = threading.Thread(target=delayed_start)
-    thread.start()
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0',port=5001)
